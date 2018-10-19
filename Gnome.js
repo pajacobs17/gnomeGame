4cd627db4e4403e30976306c4f45cf1e42468ce2
branbran902 <branbran902@gmail.com>
2018-10-18 22:10:13 -0400

    kinda made classes... also added a paul gnome

--- Gnome.js
+++ Gnome.js
@@ -1,14 +1,11 @@
-//gnome class
-function Gnome(src){
-  //generate random numbers to position Gnome
-  var posH = randNum(canvas.clientHeight);
-  var posW = randNum(canvas.clientWidth);
-
-  //create Gnome
-  //86 and 135 are the width and height of the Gnome picture
-  var aGnome = new Sprite(game, src, 40, 80);
-  aGnome.setSpeed(0);
-  aGnome.setPosition(posW, posH);
-
-  return aGnome;
-}
+function gnome(src){
+          //generate random numbers to position gnome
+          var posH = randNum(canvas.clientHeight);
+          var posW = randNum(canvas.clientWidth);
+          //create gnome
+          //86 and 135 are the width and height of the gnome picture
+          var aGnome = new Sprite(game, src, 86, 135);
+          aGnome.setSpeed(0);
+          aGnome.setPosition(posW, posH);
+          return aGnome;
+        }
--- Mower.js
+++ Mower.js
@@ -1,31 +1,28 @@
-function Mower(src){
-	
-	var Mower = new Sprite(game, src, 20, 112);
-	Mower.setSpeed(0);
-	Mower.setPosition(0, 0);
-
-	Mower.checkKeys = function(){
-		//temporary function for testing
-		if (keysDown[K_UP]){
-			this.changeYby(-5);
-			 this.setImage("img/Brandy-Back.png")
-		}
-		
-		if (keysDown[K_DOWN]){
-			this.changeYby(5);
-			 this.setImage("img/Brandy-Forward.png")
-		}
-		
-		if (keysDown[K_LEFT]){
-		   this.setImage("img/Brandy-Left.png")
-			this.changeXby(-5);
-		}
-		
-		if (keysDown[K_RIGHT]){
-			this.setImage("img/Brandy-Right.png")
-			this.changeXby(5);
-		}
-	} // end checkKeys
-
-	return Mower;
-}
+ function mower(){
+        var mower = new Sprite(game, "img/Brandy-Forward.png", 50, 112);
+        mower.setSpeed(0);
+        mower.setPosition(30, 60);
+        mower.checkKeys = function(){
+            //temporary function for testing
+            if (keysDown[K_UP]){
+                this.changeYby(-5);
+                 this.setImage("img/Brandy-Back.png")
+            }
+            
+            if (keysDown[K_DOWN]){
+                this.changeYby(5);
+                 this.setImage("img/Brandy-Forward.png")
+            }
+            
+            if (keysDown[K_LEFT]){
+               this.setImage("img/Brandy-Left.png")
+                this.changeXby(-5);
+            }
+            
+            if (keysDown[K_RIGHT]){
+                this.setImage("img/Brandy-Right.png")
+                this.changeXby(5);
+            }
+        } // end checkKeys
+        return mower;
+        }
